3f360615129e40537c739a4af53bfd78fba41d53
Moresop <gardner_christopher@wheatoncollege.edu>
2022-11-13 08:47:36 -0500

    Fixed the spotify link

--- assets/javascript.js
+++ assets/javascript.js
@@ -213,6 +213,9 @@ function combine() {
 			.then((response) => response.json())
 			.then((response) => {
 				console.log("response", response);
+				const uriNew = response.artists.items[0].data.uri
+				console.log(uriNew);
+				const num = uriNew.split(':')[2]
 				const combination = "Wind & Earth - The Rolling Stones"
 
 				// for (var i = 0; i < combinationHistory.length; i++) {
@@ -231,7 +234,7 @@ function combine() {
 				const spotify = document.getElementById("spotify");
 				const { profile, url, visuals } = response.artists.items[0].data;
 				spotify.innerHTML = `
-					<font size="+3">${profile.name}</font>
+					<a href="https://open.spotify.com/artist/${num}">${profile.name}</a>
 					<div>
 					<font size="+3">Wind and Earth</font>
 					</div>
@@ -276,6 +279,9 @@ function combine() {
 			.then((response) => response.json())
 			.then((response) => {
 				console.log("response", response);
+				const uriNew = response.artists.items[0].data.uri
+				console.log(uriNew);
+				const num = uriNew.split(':')[2]
 				const combination = "Wind & Water - Deepsky"
 
 				if (combinationHistory.indexOf(combination) === -1) {
@@ -290,7 +296,7 @@ function combine() {
 				const spotify = document.getElementById("spotify");
 				const { profile, uri, visuals } = response.artists.items[0].data;
 				spotify.innerHTML = `
-					<h2>${profile.name}</h2>
+					<a href="https://open.spotify.com/artist/${num}">${profile.name}</a>
 					<h3>Wind and Water</h3>
 					<img src="${visuals.avatarImage.sources[0].url}" style="width:500px;height:600px;" />
           <h3>This combo is the music of change, especially social change. This combination is aware of the circular nature of change and is comfortable with constant shifting, unaware of why not everyone else is. This combo is also aware of the way things tend to come back. They are aware that history is tomorrow. Its is music that escapes time itself</h3>
@@ -331,6 +337,9 @@ function combine() {
 			.then((response) => response.json())
 			.then((response) => {
 				console.log("response", response);
+				const uriNew = response.artists.items[0].data.uri
+				console.log(uriNew);
+				const num = uriNew.split(':')[2]
 				const combination = "Wind & Fire - Red Hot Chili Peppers"
 
 				if (combinationHistory.indexOf(combination) === -1) {
@@ -344,7 +353,7 @@ function combine() {
 				const spotify = document.getElementById("spotify");
 				const { profile, uri, visuals } = response.artists.items[0].data;
 				spotify.innerHTML = `
-					<h2>${profile.name}</h2>
+					<a href="https://open.spotify.com/artist/${num}">${profile.name}</a>
 					<h3>Wind and Fire</h3>
 					<img src="${visuals.avatarImage.sources[0].url}" style="width:500px;height:600px;"/>
           <h3>This is an abstract combination, an ideological rampage. It is the witch hunt, death camps, and the incited revolution. It’s mass fervor and rage. It’s the music of a great, big horde of people all getting to describe something the same way, with the same slogans and words, and going out to do something about their sense of frustration.</h3>
@@ -384,6 +393,9 @@ function combine() {
 			.then((response) => response.json())
 			.then((response) => {
 				console.log("response", response);
+				const uriNew = response.artists.items[0].data.uri
+				console.log(uriNew);
+				const num = uriNew.split(':')[2]
 				const combination = "Wind & Dark - TOBACCO"
 
 				if (combinationHistory.indexOf(combination) === -1) {
@@ -398,7 +410,7 @@ function combine() {
 				const spotify = document.getElementById("spotify");
 				const { profile, uri, visuals } = response.artists.items[0].data;
 				spotify.innerHTML = `
-					<h2>${profile.name}</h2>
+					<a href="https://open.spotify.com/artist/${num}">${profile.name}</a>
 					<h3>Wind and Dark</h3>
 					<img src="${visuals.avatarImage.sources[0].url}" style="width:500px;height:600px;"/>
           <h3>This is a warped combo, the music of an atelectatic lung in a great machine world whose design and purpose is so beyond our understanding it is supernatural. Fear and confusion are all that awaits you. But, that is still better than feeling nothing at all</h3>
@@ -438,6 +450,9 @@ function combine() {
 			.then((response) => response.json())
 			.then((response) => {
 				console.log("response", response);
+				const uriNew = response.artists.items[0].data.uri
+				console.log(uriNew);
+				const num = uriNew.split(':')[2]
 				const combination = "Wind & Light - The White Stripes"
 
 				if (combinationHistory.indexOf(combination) === -1) {
@@ -452,7 +467,7 @@ function combine() {
 				const spotify = document.getElementById("spotify");
 				const { profile, uri, visuals } = response.artists.items[0].data;
 				spotify.innerHTML = `
-					<h2>${profile.name}</h2>
+					<a href="https://open.spotify.com/artist/${num}">${profile.name}</a>
 					<h3>Wind and Light</h3>
 					<img src="${visuals.avatarImage.sources[0].url}" style="width:500px;height:600px;"/>
           <h3>The combo of freedom. Where you are flying through a clear bright sky where anything is possible. This is the music of dreams. Reality has no power in such a combination.</h3>
@@ -492,6 +507,9 @@ function combine() {
 			.then((response) => response.json())
 			.then((response) => {
 				console.log("response", response);
+				const uriNew = response.artists.items[0].data.uri
+				console.log(uriNew);
+				const num = uriNew.split(':')[2]
 				const combination = "Earth & Fire - The Comet is Coming"
 
 				if (combinationHistory.indexOf(combination) === -1) {
@@ -506,7 +524,7 @@ function combine() {
 				const spotify = document.getElementById("spotify");
 				const { profile, uri, visuals } = response.artists.items[0].data;
 				spotify.innerHTML = `
-					<h2>${profile.name}</h2>
+					<a href="https://open.spotify.com/artist/${num}">${profile.name}</a>
 					<h3>Earth and Fire</h3>
 					<img src="${visuals.avatarImage.sources[0].url}" style="width:500px;height:600px;"/>
           <h3>The combo of creation, the titanic power and scale of the universe in which we are mere specks. What is the meaning of such life in the face of titanic forces? This is the music of Existentialism</h3>
@@ -548,6 +566,9 @@ function combine() {
 			.then((response) => response.json())
 			.then((response) => {
 				console.log("response", response);
+				const uriNew = response.artists.items[0].data.uri
+				console.log(uriNew);
+				const num = uriNew.split(':')[2]
 				const combination = "Earth & Water - The Heavy"
 
 				if (combinationHistory.indexOf(combination) === -1) {
@@ -562,7 +583,7 @@ function combine() {
 				const spotify = document.getElementById("spotify");
 				const { profile, uri, visuals } = response.artists.items[0].data;
 				spotify.innerHTML = `
-					<h2>${profile.name}</h2>
+					<a href="https://open.spotify.com/artist/${num}">${profile.name}</a>
 					<h3>Earth and Water</h3>
 					<img src="${visuals.avatarImage.sources[0].url}" style="width:500px;height:600px;"/>
           <h3>This combo is the sound of silence, peace and solitude in a world that insists you engage with it. In our times, women have always been left out of heady topics such as revolution and nationalism. Queers and people of color are anti-nationalist simply by existing. The people of earth and water are just here and that act does not need to be revolutionary to be just.</h3>
@@ -602,6 +623,9 @@ function combine() {
 			.then((response) => response.json())
 			.then((response) => {
 				console.log("response", response);
+				const uriNew = response.artists.items[0].data.uri
+				console.log(uriNew);
+				const num = uriNew.split(':')[2]
 				const combination = "Earth & Dark - MF Doom"
 
 				if (combinationHistory.indexOf(combination) === -1) {
@@ -616,7 +640,7 @@ function combine() {
 				const spotify = document.getElementById("spotify");
 				const { profile, uri, visuals } = response.artists.items[0].data;
 				spotify.innerHTML = `
-					<h2>${profile.name}</h2>
+					<a href="https://open.spotify.com/artist/${num}">${profile.name}</a>
 					<h3>Earth and Dark</h3>
 					<img src="${visuals.avatarImage.sources[0].url}" style="width:500px;height:600px;"/>
           <h3>The combo of reality. Earth and Dark is the here and now, the struggle of all life and the beauty of the mundane. The music of community and the collective experience of society both good and bad</h3>
@@ -658,6 +682,9 @@ function combine() {
 			.then((response) => response.json())
 			.then((response) => {
 				console.log("response", response);
+				const uriNew = response.artists.items[0].data.uri
+				console.log(uriNew);
+				const num = uriNew.split(':')[2]
 				const combination = "Earth & Light - Planet Waves"
 
 				if (combinationHistory.indexOf(combination) === -1) {
@@ -672,7 +699,7 @@ function combine() {
 				const spotify = document.getElementById("spotify");
 				const { profile, uri, visuals } = response.artists.items[0].data;
 				spotify.innerHTML = `
-					<h2>${profile.name}</h2>
+					<a href="https://open.spotify.com/artist/${num}">${profile.name}</a>
 					<h3>Earth and Light</h3>
 					<img src="${visuals.avatarImage.sources[0].url}" style="width:500px;height:600px;"/>
           <h3>The combo of revelations, the light cracks the stone and all things kept underground are brought to light. This is the music of those that seek truths and the agony they often bring upon themselves.</h3>
@@ -714,6 +741,9 @@ function combine() {
 			.then((response) => response.json())
 			.then((response) => {
 				console.log("response", response);
+				const uriNew = response.artists.items[0].data.uri
+				console.log(uriNew);
+				const num = uriNew.split(':')[2]
 				const combination = "Water & Fire - Hillsong United"
 
 				if (combinationHistory.indexOf(combination) === -1) {
@@ -728,7 +758,7 @@ function combine() {
 				const spotify = document.getElementById("spotify");
 				const { profile, uri, visuals } = response.artists.items[0].data;
 				spotify.innerHTML = `
-					<h2>${profile.name}</h2>
+					<a href="https://open.spotify.com/artist/${num}">${profile.name}</a>
 					<h3>Water and Fire</h3>
 					<img src="${visuals.avatarImage.sources[0].url}" style="width:500px;height:600px;"/>
           <h3>This is a combo of instant reaction, of gut feeling, of always moving and always decision making. It is a combo of always burning yourself out because water drowns fire and fire evaporates water. The music of Fire and Water is never still. It writhes under the skin and rings in the ears. chaos is its natural state.</h3>
@@ -771,6 +801,9 @@ function combine() {
 			.then((response) => response.json())
 			.then((response) => {
 				console.log("response", response);
+				const uriNew = response.artists.items[0].data.uri
+				console.log(uriNew);
+				const num = uriNew.split(':')[2]
 				const combination = "Water & Dark - Agent Fresco"
 
 				if (combinationHistory.indexOf(combination) === -1) {
@@ -785,7 +818,7 @@ function combine() {
 				const spotify = document.getElementById("spotify");
 				const { profile, uri, visuals } = response.artists.items[0].data;
 				spotify.innerHTML = `
-					<h2>${profile.name}</h2>
+					<a href="https://open.spotify.com/artist/${num}">${profile.name}</a>
 					<h3>Water and Dark</h3>
 					<img src="${visuals.avatarImage.sources[0].url}" style="width:500px;height:600px;"/>
           <h3>The combo of peace. Dark has many negative connotations, but it can also be deep, safe and very gentle energy. The deep dark water returns us to the place we were before we were. The infinite amount of time where we did not exist.  It is the music of a great deep sea that welcomes all.</h3>
@@ -825,6 +858,9 @@ function combine() {
 			.then((response) => response.json())
 			.then((response) => {
 				console.log("response", response);
+				const uriNew = response.artists.items[0].data.uri
+				console.log(uriNew);
+				const num = uriNew.split(':')[2]
 				const combination = "Water & Light - Creedence Clearwater Revival"
 
 				if (combinationHistory.indexOf(combination) === -1) {
@@ -839,7 +875,7 @@ function combine() {
 				const spotify = document.getElementById("spotify");
 				const { profile, uri, visuals } = response.artists.items[0].data;
 				spotify.innerHTML = `
-					<h2>${profile.name}</h2>
+					<a href="https://open.spotify.com/artist/${num}">${profile.name}</a>
 					<h3>Water and Light</h3>
 					<img src="${visuals.avatarImage.sources[0].url}" style="width:500px;height:600px;"/>
           <h3>The combo of life. Water and light is a divine convergence where all things become balanced and all truths are made clear. The music of negative energy being washed away</h3>
@@ -881,6 +917,9 @@ function combine() {
 			.then((response) => response.json())
 			.then((response) => {
 				console.log("response", response);
+				const uriNew = response.artists.items[0].data.uri
+				console.log(uriNew);
+				const num = uriNew.split(':')[2]
 				const combination = "Fire & Dark - Cigarettes After Sex"
 
 				if (combinationHistory.indexOf(combination) === -1) {
@@ -895,7 +934,7 @@ function combine() {
 				const spotify = document.getElementById("spotify");
 				const { profile, uri, visuals } = response.artists.items[0].data;
 				spotify.innerHTML = `
-					<h2>${profile.name}</h2>
+					<a href="https://open.spotify.com/artist/${num}">${profile.name}</a>
 					<h3>Fire and Dark</h3>
 					<img src="${visuals.avatarImage.sources[0].url}" style="width:500px;height:600px;"/>
           <h3>A fire that produces no heat and casts no shadow. This combination is the opposite of existence, an endless void. This is the music of nothing, the heat death of the universe where all energy is spent and nothing can ever exist again.</h3>
@@ -936,6 +975,9 @@ function combine() {
 			.then((response) => response.json())
 			.then((response) => {
 				console.log("response", response);
+				const uriNew = response.artists.items[0].data.uri
+				console.log(uriNew);
+				const num = uriNew.split(':')[2]
 				const combination = "Fire & Light - Empire of the Sun"
 
 				if (combinationHistory.indexOf(combination) === -1) {
@@ -950,7 +992,7 @@ function combine() {
 				const spotify = document.getElementById("spotify");
 				const { profile, uri, visuals } = response.artists.items[0].data;
 				spotify.innerHTML = `
-					<h2>${profile.name}</h2>
+					<a href="https://open.spotify.com/artist/${num}">${profile.name}</a>
 					<h3>Fire and Light</h3>
 					<img src="${visuals.avatarImage.sources[0].url}" style="width:500px;height:600px;"/>
           <h3>A powerful combo that is as bright and brilliant as the sun. It burns bright and fades instantly. This is the music of miracles.</h3>
@@ -991,6 +1033,9 @@ function combine() {
 			.then((response) => response.json())
 			.then((response) => {
 				console.log("response", response);
+				const uriNew = response.artists.items[0].data.uri
+				console.log(uriNew);
+				const num = uriNew.split(':')[2]
 				const combination = "Dark & Light - The Midnight"
 
 				if (combinationHistory.indexOf(combination) === -1) {
@@ -1005,7 +1050,7 @@ function combine() {
 				const spotify = document.getElementById("spotify");
 				const { profile, uri, visuals } = response.artists.items[0].data;
 				spotify.innerHTML = `
-					<h2>${profile.name}</h2>
+					<a href="https://open.spotify.com/artist/${num}">${profile.name}</a>
 					<h3>Dark and Light</h3>
 					<img src="${visuals.avatarImage.sources[0].url}" style="width:500px;height:600px;"/>
           <h3>A perfectly balanced combination, the brightest of lights cast the darkest of shadows. This is the music of nuance and morality.</h3>
@@ -1043,26 +1088,4 @@ function combine() {
 	}
 }
 
-//search function
-//Sys.setenv((SPOTIFY_CLIENT_ID = "96dc2bc1c09645d8af8883911f9c6ae7"));
-//Sys.setenv((SPOTIFY_CLIENT_SECRET = "d6b103e15d1e41758ae6fefc9cfdec8d"));
-
-
-const form = document.querySelector('#inputForm');
-form.addEventListener("submit", e => {
-	e.preventDefault();
-	const input = document.querySelector("#searchText");
-	let inputVal = input.value;
-
-	//ajax here
-	const url = `https://spotify23.p.rapidapi.com/search/?q=${inputVal}&type=multi&offset=0&limit=10&numberOfTopResults=5`;
-
-	fetch(url, options)
-		.then((response) => response.json())
-		.then((response) => {
-			console.log("response", response);
-
-		})
-		.catch((err) => console.error(err));
-})
 
